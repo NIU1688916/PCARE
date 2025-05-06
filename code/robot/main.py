f658b2e890c795245e169b32eea9922562a8a413
Raúl Jiménez <1688916@uab.cat>
2025-05-06 15:06:58 +0200

    primera aproximacion funciones

--- code/robot/main.py
+++ code/robot/main.py
@@ -1,1 +1,47 @@
-//Declaracion de funciones
+#Declaracion de funciones
+
+#APP Mobil
+def detectar_tipo_planta(imagen):
+    pass
+
+def enviar_datos_al_robot(datos):
+    pass 
+
+
+#Conexion con la app
+def recibir_datos_app():
+    pass
+
+def adaptar_funcionamiento_robot(datos):
+    pass
+
+
+#Mapeo habitacion a partir de la camara
+def capturar_imagen_camara():
+    pass
+
+def generar_mapeo_habitacion(imagen):
+    pass
+
+
+#Sensores
+def leer_sensor_ultrasonido():
+    pass
+
+def leer_sensor_luminosidad():
+    pass
+
+def leer_sensor_humedad():
+    pass
+
+def configurar_sensores():
+    pass
+
+
+
+#Control de motores
+def activar_motor_movimiento(direccion, velocidad):
+    pass
+
+def activar_bomba_agua(tiempo):
+    pass
